bf1468d37148281b934840aa52b94938ed69f0db
Jewdeer21 <ayecohen21@gmail.com>
2019-06-12 15:32:45 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      classes: [
+        {name: "Warrior", votes: 0},
+        {name: "Mage", votes: 0},
+        {name: "Archer", votes: 0},
+        {name: "Priest", votes: 0},
+        {name: "Bard", votes: 0},
+        {name: "Rogue", votes: 0}
+      ]
+    };
+  }
+
+  vote(i) {
+    console.log("Voting has occured", i);
+    let c = [...this.state.classes];
+    c[i].votes++;
+    this.setState({classes: c});
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Best RPG Classes</h1>
+        {
+          this.state.classes.map( (c, i) =>
+            <div key={i} className="class">
+              <h4>{c.name}</h4>
+              <button onClick={this.vote.bind(this, i)}>Likes: {c.votes}</button>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+  
 }
 
 export default App;
